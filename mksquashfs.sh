4fd242f8500d49d5bb8d738aa5995e430c524e70
Svet Bajlekov <tomatoman@gmail.com>
2023-04-10 15:29:15 +0200

    script for exporting root partition as SquashFS

---
+++ mksquashfs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+
+# Mount last created image
+IMAGE_FILE="$(ls -1 deploy/*.img | tail -1)"
+LOOPDEV=$(losetup -Pf --show $IMAGE_FILE)
+TMPROOT=$(mktemp -d)
+mount ${LOOPDEV}p2 $TMPROOT
+
+# Create squashfs
+SQFS_FILE="${IMAGE_FILE%.*}.sqfs"
+mksquashfs $TMPROOT $SQFS_FILE -noappend -no-exports -no-fragments -all-root -all-time 0 -mkfs-time 0 -noI -noD -noF -noX
+
+# Cleanup
+umount $TMPROOT
+rmdir $TMPROOT
+losetup -d $LOOPDEV
