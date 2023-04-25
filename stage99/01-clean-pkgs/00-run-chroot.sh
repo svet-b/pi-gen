42e0471306d23dc512f15c92d19031e2f5e746ac
Svet Bajlekov <tomatoman@gmail.com>
2023-04-25 18:42:32 +0200

    remove e2fsprogs

--- stage99/01-clean-pkgs/00-run-chroot.sh
+++ stage99/01-clean-pkgs/00-run-chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-apt remove --purge -y busybox rsyslog raspi-config
+apt remove --purge -y --allow-remove-essential busybox e2fsprogs rsyslog raspi-config
 apt autoremove --purge -y
 apt clean
